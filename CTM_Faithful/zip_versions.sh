6d7a7240bcd04b66c645936b043b10ba8a7935b6
aeldit <raphael.roger.92@gmail.com>
2024-05-03 17:48:54 +0200

    Fix the zip scripts

--- CTM_Faithful/zip_versions.sh
+++ CTM_Faithful/zip_versions.sh
@@ -9,14 +9,15 @@ if [ -f "pack.mcmeta" ]; then
 	rm "pack.mcmeta"
 fi
 
-rm CTM\ OF-Fabric*.zip
+# Removes any zip archive that is still in the directory
+rm CTM\ Faithful*.zip
 
 # Obtains the MC version for each file
 VERSIONS=$(ls *.mcmeta | cut -c 6-11)
 
 for ver in $VERSIONS; do
 	cp "pack_$ver.mcmeta" pack.mcmeta
-	zip -r "CTM OF-Fabric $1+$ver.zip" assets/ LICENSE.txt CREDITS.txt pack.png pack.mcmeta
+	zip -r "CTM Faithful $1+$ver.zip" assets/ LICENSE.txt CREDITS.txt pack.png pack.mcmeta
 done
 
 rm "pack.mcmeta"
