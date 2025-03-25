bd1ae7295d6b8092e0aee1998a07784cdab5fc5f
aeldit <aeldit.66@proton.me>
2025-03-25 19:56:02 +0200

    Fix wrong status code

--- Dark Smooth GUI/publish.py
+++ Dark Smooth GUI/publish.py
@@ -160,7 +160,7 @@ def publish(
         files={file: open(file, "rb")},
     )
 
-    if req.status_code == 204:
+    if req.status_code == 200:
         print(f"Successfully uploaded version {version}+{mc_version}")
     else:
         print(
@@ -246,7 +246,7 @@ def main(version: str, changelog: str) -> None:
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) != 2:
+    if len(args) < 2:
         print("Version not specified; Aborting")
         exit(1)
 
